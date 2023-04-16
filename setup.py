373c6be6a4f4088807878733dfb288d53319c7b1
Ellariel <d.v.valko@gmail.com>
2023-04-16 02:20:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,17 +7,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='meta_utils',
+    name='meta_tools',
     version='1.0',
     author='Ellariel',
     author_email='d.v.valko@gmail.com',
-    description="Some utils for meta-analysis and reporting with APA standards.",
+    description="Some tools for meta-analysis and reporting of research results according to APA standards.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/ellariel/meta_utils',
+    url='https://github.com/ellariel/meta_tools',
     license='MIT',
     include_package_data=True,
-    package_data={'meta_utils': ['meta_utils/*.py']},
-    packages=['meta_utils'],
+    package_data={'meta_tools': ['meta_tools/*.py']},
+    packages=['meta_tools'],
     install_requires=required,
 )
