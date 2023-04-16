0ecd17759ac92e37fa3a6a0df5577dad9c095fe0
Ellariel <ell.science@mail.ru>
2023-04-16 02:04:38 +0200

    m

--- setup.py
+++ setup.py
@@ -7,17 +7,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='meta_analysis_utils',
+    name='meta_utils',
     version='1.0',
     author='Ellariel',
     author_email='d.v.valko@gmail.com',
     description="Some utils for meta-analysis and reporting with APA standards.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/ellariel/meta-analysis-utils',
+    url='https://github.com/ellariel/meta_utils',
     license='MIT',
     include_package_data=True,
-    package_data={'meta_analysis_utils': ['meta_analysis_utils/*.py']},
-    packages=['meta_analysis_utils'],
+    package_data={'meta_utils': ['meta_utils/*.py']},
+    packages=['meta_utils'],
     install_requires=required,
 )
