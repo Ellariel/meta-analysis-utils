108877667dbc946cabdbd7c410990d208c09deed
Ellariel <ell.science@mail.ru>
2023-04-30 15:53:24 +0200

    m

--- meta_tools/report.py
+++ meta_tools/report.py
@@ -16,7 +16,7 @@ from scipy.stats import wilcoxon, mannwhitneyu
 from scipy.stats import pearsonr, spearmanr
 
 ######################################################################################
-
+# https://cran.r-project.org/web/packages/effectsize/vignettes/interpret.html
 ### FORMAT
 def get_stars(p, p001='***', p01='**', p05='*', p10=''):
     if p < 0.001:
@@ -55,40 +55,40 @@ def spearman_CI(r, n, p=0.95):
     d = z_from_p(1-p) * se
     return np.array([math.tanh(math.atanh(r) - d), math.tanh(math.atanh(r) + d)])
 
-def t_from_b(b, se):
+def t_from_b(b, se): # t-statistic from beta-coefficient
     return b/se
 
-def t_from_z(z):
+def t_from_z(z): # t-statistic from Fisher's z
     # https://web.cortland.edu/andersmd/STATS/stdscore.html#:~:text=As%20evidenced%20above%2C%20zscores%20are,a%20T%20score%20of%2025.
     return 10*z + 50
 
-def z_from_t(t):
-    # https://web.cortland.edu/andersmd/STATS/stdscore.html#:~:text=As%20evidenced%20above%2C%20zscores%20are,a%20T%20score%20of%2025.
-    return (t - 50) / 10 
-
-def z_from_p(p, method='two-tailed'):
+def z_from_p(p, method='two-tailed'): # Fisher's z from p-value
     # https://www.gigacalculator.com/calculators/p-value-to-z-score-calculator.php
     z = scipy.stats.norm.ppf(p/2) if method == 'two-tailed' else scipy.stats.norm.ppf(p)
     return -z
 
-def cohen_r_from_p(p, n, method='two-tailed'):
+def cohen_r_from_p(p, n, method='two-tailed'): # Cohen’s r from p-value
     # Cohen’s guidelines for r are that a large effect is .5, a medium effect is .3, and a small effect is .1 (Coolican, 2009, p. 395).
     # Fritz, C. O., Morris, P. E., & Richler, J. J. (2012). Effect size estimates: Current use, calculations, and interpretation. Journal of Experimental Psychology: General, 141(1), 2–18. https://doi.org/10.1037/a0024338
     # https://www.researchgate.net/publication/51554230_Effect_Size_Estimates_Current_Use_Calculations_and_Interpretation
     return z_from_p(p, method=method) / math.sqrt(n)
     
-def cohen_r_from_z(z, n):
+def cohen_r_from_z(z, n): # Cohen’s r from Fisher's z 
     # Cohen’s guidelines for r are that a large effect is .5, a medium effect is .3, and a small effect is .1 (Coolican, 2009, p. 395).
     # Fritz, C. O., Morris, P. E., & Richler, J. J. (2012). Effect size estimates: Current use, calculations, and interpretation. Journal of Experimental Psychology: General, 141(1), 2–18. https://doi.org/10.1037/a0024338
     # https://www.researchgate.net/publication/51554230_Effect_Size_Estimates_Current_Use_Calculations_and_Interpretation
     return z / math.sqrt(n)
 
-def cohen_d_from_t(t, n):
+def cohen_d_from_z(z, n): # Cohen’s d from Fisher's z 
+    # https://easystats.github.io/effectsize/reference/t_to_r.html
+    return 2*z / math.sqrt(n)
+
+def cohen_d_from_t(t, n): # Cohen’s d from t-statistic
     # Fritz, C. O., Morris, P. E., & Richler, J. J. (2012). Effect size estimates: Current use, calculations, and interpretation. Journal of Experimental Psychology: General, 141(1), 2–18. https://doi.org/10.1037/a0024338
     # https://www.researchgate.net/publication/51554230_Effect_Size_Estimates_Current_Use_Calculations_and_Interpretation
     return 2*t / math.sqrt(n-2)
 
-def cohen_d_from_f(f, df_num, df_denom):
+def cohen_d_from_f(f, df_num, df_denom): # Cohen’s d from F-statistic
     # Cohen’s d derived from F-statistic (Fritz et al., 2012)
     # Fritz, C. O., Morris, P. E., & Richler, J. J. (2012). Effect size estimates: Current use, calculations, and interpretation. Journal of Experimental Psychology: General, 141(1), 2–18. https://doi.org/10.1037/a0024338
     # https://www.researchgate.net/publication/51554230_Effect_Size_Estimates_Current_Use_Calculations_and_Interpretation
@@ -96,6 +96,17 @@ def cohen_d_from_f(f, df_num, df_denom):
     eta_sq = (f * df_num) / (f * df_num + df_denom)
     d = 2 * math.sqrt(eta_sq) / math.sqrt(1 - eta_sq)
     return d
+
+def z_from_r(r): # Fisher's z from correlation r
+    # https://www.escal.site/
+    return (np.log(1 + r) - np.log(1 - r)) / 2
+
+def logOR_from_cohen_d(d): # log odds ratio from Cohen's d
+    # https://www.escal.site/
+    return np.pi*d / math.sqrt(3)
+
+def r_from_cohen_d(d): # Pearson's r from Cohen's d
+    return d / math.sqrt(d*d + 4)
    
 ### OLS   
 def ols_APA(ols): # R² = .34, F(1, 416) = 6.71, p = .009
@@ -174,4 +185,4 @@ def plot_ecdf(data, xlabel=None, ylabel=None):
         plt.ylabel("ECDF")
     else:
         plt.ylabel(ylabel)
-    plt.show()
+    #plt.show()
