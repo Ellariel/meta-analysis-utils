93ee5c5a3af61c68e811dabf87118e6d9794ed31
Danila Valko <d.v.valko@gmail.com>
2024-10-15 14:30:37 +0200

    Update report.py

--- metatools/report.py
+++ metatools/report.py
@@ -56,12 +56,12 @@ def format_p(p, add_p=True, keep_space=True):
 
 
 def format_r(r):
-    if abs(r) >= 0.999:
+    if not np.isfinite(r):
+        r = 'inf'
+    elif abs(r) >= 0.999:
         r = '1.00'
     elif abs(r) < 0.005:
         r = '0.00'
-    elif not np.isfinite(r):
-        r = 'inf'
     else:
         r = f"{'-' if r < 0.0 else ''}"+f"{abs(r):.2f}"[1:]
     return r
