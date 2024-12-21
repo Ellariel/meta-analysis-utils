3709328bd4849f59b719710c25a97c901d2b58e4
Danila <d.v.valko@gmail.com>
2024-12-21 23:16:19 +0100

    modified:   metatools/calc.py 	modified:   tests/test_calc.py 	modified:   tests/test_lm.py

--- metatools/calc.py
+++ metatools/calc.py
@@ -1,6 +1,4 @@
 import numpy as np
-import random
-import math
 import scipy
 
 
@@ -9,68 +7,75 @@ import scipy
 ##############
 
 
-def d_from_r(r): 
+def d_from_r(r):
     # https://stats.stackexchange.com/questions/390549/converting-between-correlation-and-effect-size-cohens-d
     # assume equal groups
     # unequal here https://stats.stackexchange.com/questions/526789/convert-correlation-r-to-cohens-d-unequal-groups-of-known-size
-    
-    return 2 * r / math.sqrt(1 - r*r)
 
-def r_from_d(d): 
+    return 2 * r / np.sqrt(1 - r * r)
+
+
+def r_from_d(d):
     # Сorrelation r from Cohen's d
     # https://www.escal.site/
     # https://easystats.github.io/effectsize/reference/d_to_r.html
     # https://www.statisticshowto.com/probability-and-statistics/statistics-definitions/cohens-d/
-    
-    return d / math.sqrt(d*d + 4) # assume equal groups
+
+    return d / np.sqrt(d * d + 4)  # assume equal groups
+
 
 def t_from_r(r, n):
     # https://stats.stackexchange.com/questions/400146/how-to-derive-the-formula-of-the-t-test-of-the-correlation-coefficient
     # t-critical = stats.t.ppf(alpha/numOfTails, ddof)
-    
+
     df = n - 2
-    t = r / math.sqrt((1 - r*r) / df)
+    t = r / np.sqrt((1 - r * r) / df)
     return t
 
-def r_from_t(t, n): 
+
+def r_from_t(t, n):
     # Сorrelation r from t-statistic
     # https://juls-dotcom.github.io/meta_analysis.html
-    
-    df = n - 2
-    return t / math.sqrt(t*t + df)
 
-def d_from_t(t, n): 
+    df = n - 2
+    return t / np.sqrt(t * t + df)
+
+
+def d_from_t(t, n):
     # Cohen’s d from t-statistic
     # Fritz, C. O., Morris, P. E., & Richler, J. J. (2012). Effect size estimates: Current use, calculations, and interpretation. Journal of Experimental Psychology: General, 141(1), 2–18. https://doi.org/10.1037/a0024338
     # https://www.researchgate.net/publication/51554230_Effect_Size_Estimates_Current_Use_Calculations_and_Interpretation
-    
+
     df = n - 2
-    return 2 * t / math.sqrt(df)
+    return 2 * t / np.sqrt(df)
+
 
 def t_from_d(d, n):
     # Cohen’s d from t-statistic
     # Fritz, C. O., Morris, P. E., & Richler, J. J. (2012). Effect size estimates: Current use, calculations, and interpretation. Journal of Experimental Psychology: General, 141(1), 2–18. https://doi.org/10.1037/a0024338
     # https://www.researchgate.net/publication/51554230_Effect_Size_Estimates_Current_Use_Calculations_and_Interpretation
-    
-    df = n - 2
-    return d * math.sqrt(df) / 2
 
-def z_from_r(r): 
+    df = n - 2
+    return d * np.sqrt(df) / 2
+
+
+def z_from_r(r):
     # Fisher's z from correlation r
     # https://www.escal.site/
     # https://en.wikipedia.org/wiki/Fisher_transformation
     # np.log((1 + r)/(1 - r)) / 2
-    
-    return math.atanh(r)
 
-def r_from_z(z): 
-    # Сorrelation r (Cohen’s r) from Fisher's z 
+    return np.atanh(r)
+
+
+def r_from_z(z):
+    # Сorrelation r (Cohen’s r) from Fisher's z
     # https://www.escal.site/
     # https://www.statisticshowto.com/fisher-z/
     # https://en.wikipedia.org/wiki/Inverse_hyperbolic_functions
-    
-    r = math.sqrt(1 - math.pow(1 / math.cosh(z), 2))
-    r = math.copysign(r, z)
+
+    r = np.sqrt(1 - np.pow(1 / np.cosh(z), 2))
+    r = np.copysign(r, z)
     return r
 
 
@@ -79,65 +84,68 @@ def r_from_z(z):
 ###########
 
 
-def z_from_p(p, method='two-tailed'): 
+def z_from_p(p, method="two-tailed"):
     # Fisher's z from p-value
     # z-critical = stats.norm.ppf(1 - alpha) (use alpha = alpha/2 for two-sided)
     # https://www.gigacalculator.com/calculators/p-value-to-z-score-calculator.php
     # https://stackoverflow.com/questions/20864847/probability-to-z-score-and-vice-versa
-    
-    p = p / 2 if method == 'two-tailed' else p
+
+    p = p / 2 if method == "two-tailed" else p
     z = scipy.stats.norm.ppf(p)
-    return abs(z)
+    return np.abs(z)
 
-def p_from_z(z, method='two-tailed'): 
+
+def p_from_z(z, method="two-tailed"):
     # z-critical = stats.norm.ppf(1 - alpha) (use alpha = alpha/2 for two-sided)
     # https://stackoverflow.com/questions/20864847/probability-to-z-score-and-vice-versa
-    
-    p = scipy.stats.norm.sf(abs(z))
-    p = p * 2 if method == 'two-tailed' else p
+
+    p = scipy.stats.norm.sf(np.abs(z))
+    p = p * 2 if method == "two-tailed" else p
     return p
 
-def p_from_t(t, n, method='two-tailed'): 
+
+def p_from_t(t, n, method="two-tailed"):
     # z-critical = stats.norm.ppf(1 - alpha) (use alpha = alpha/2 for two-sided)
     # t-critical = stats.t.ppf(alpha/numOfTails, ddof)
     # https://stackoverflow.com/questions/20864847/probability-to-z-score-and-vice-versa
-    
+
     df = n - 2
-    p = scipy.stats.t.sf(abs(t), df)
-    p = p * 2 if method == 'two-tailed' else p
+    p = scipy.stats.t.sf(np.abs(t), df)
+    p = p * 2 if method == "two-tailed" else p
     return p
 
-def t_from_p(p, n, method='two-tailed'): 
+
+def t_from_p(p, n, method="two-tailed"):
     # z-critical = stats.norm.ppf(1 - alpha) (use alpha = alpha/2 for two-sided)
     # t-critical = stats.t.ppf(alpha/numOfTails, ddof)
     # https://stackoverflow.com/questions/20864847/probability-to-z-score-and-vice-versa
-    
+
     df = n - 2
-    p = p / 2 if method == 'two-tailed' else p
+    p = p / 2 if method == "two-tailed" else p
     t = scipy.stats.t.ppf(p, df)
-    return abs(t)
+    return np.abs(t)
 
-def r_from_p(p, n, method='two-tailed'): 
+
+def r_from_p(p, n, method="two-tailed"):
     # Cohen’s r from p-value
     # Cohen’s guidelines for r are that a large effect is .5, a medium effect is .3, and a small effect is .1 (Coolican, 2009, p. 395).
     # Fritz, C. O., Morris, P. E., & Richler, J. J. (2012). Effect size estimates: Current use, calculations, and interpretation. Journal of Experimental Psychology: General, 141(1), 2–18. https://doi.org/10.1037/a0024338
     # https://www.researchgate.net/publication/51554230_Effect_Size_Estimates_Current_Use_Calculations_and_Interpretation
-    
+
     r = r_from_t(t_from_p(p, n, method=method), n)
-    return r # z_from_p(p, method=method) / math.sqrt(n)
+    return r  # z_from_p(p, method=method) / math.sqrt(n)
 
-def p_from_r(r, n, method='two-tailed'): 
-    
+
+def p_from_r(r, n, method="two-tailed"):
     p = p_from_t(t_from_r(r, n), n, method=method)
-    return p # z_from_p(p, method=method) / math.sqrt(n)
+    return p  # z_from_p(p, method=method) / math.sqrt(n)
 
 
-def d_from_p(p, n, method='two-tailed'): 
-    
+def d_from_p(p, n, method="two-tailed"):
     return d_from_r(r_from_p(p, n, method=method))
 
-def p_from_d(d, n, method='two-tailed'): 
-    
+
+def p_from_d(d, n, method="two-tailed"):
     return p_from_r(r_from_d(d), n, method=method)
 
 
@@ -146,20 +154,21 @@ def p_from_d(d, n, method='two-tailed'):
 ##############
 
 
-def z_from_t(t, n, method=None): 
+def z_from_t(t, n, method=None):
     # t-statistic from Fisher's z
     # https://web.cortland.edu/andersmd/STATS/stdscore.html#:~:text=As%20evidenced%20above%2C%20zscores%20are,a%20T%20score%20of%2025.
-    
+
     if method:
         p = p_from_t(t, n, method=method)
         z = z_from_p(p, method=method)
         return z
     return z_from_r(r_from_t(t, n))
 
-def t_from_z(z, n, method=None): 
+
+def t_from_z(z, n, method=None):
     # t-statistic from Fisher's z
     # https://web.cortland.edu/andersmd/STATS/stdscore.html#:~:text=As%20evidenced%20above%2C%20zscores%20are,a%20T%20score%20of%2025.
-    
+
     if method:
         p = p_from_z(z, method=method)
         t = t_from_p(p, n, method=method)
@@ -170,44 +179,15 @@ def t_from_z(z, n, method=None):
 def d_from_z(z):
     # https://easystats.github.io/effectsize/reference/t_to_r.html
     # assumed equal groups
-    
+
     return d_from_r(r_from_z(z))
+
 
 def z_from_d(d):
     # https://easystats.github.io/effectsize/reference/t_to_r.html
     # assumed equal groups
-    
+
     return z_from_r(r_from_d(d))
-
-
-##############
-# Cohen r, d #
-##############
-
-
-def cohen_r_from_p(p, n, method='two-tailed'): 
-    return r_from_p(p, n, method=method)
-
-def cohen_r_from_d(d): 
-    return r_from_d(d)
-
-def cohen_r_from_z(z): 
-    return r_from_z(z)
-
-def cohen_r_from_t(t, n):
-    return r_from_t(t, n)
-
-def cohen_d_from_p(p, n, method='two-tailed'): 
-    return d_from_p(p, n, method=method)
-
-def cohen_d_from_r(r): 
-    return d_from_r(r)
-
-def cohen_d_from_z(z): 
-    return d_from_z(z)
-
-def cohen_d_from_t(t, n):
-    return d_from_t(t, n)
 
 
 ########################
@@ -215,86 +195,116 @@ def cohen_d_from_t(t, n):
 ########################
 
 
-
-
-
-def CI_for_m(data, func=np.mean, p=0.95, n=1000, seed=13):
-    '''
-    Bootstraping confidence intervals for the mean/median value
-    
-    https://towardsdatascience.com/how-to-calculate-confidence-intervals-in-python-a8625a48e62b
-    '''
-
-    random.seed(seed)
-    np.random.seed(seed)
-    simulations = [func(np.random.choice(data, size=len(data), replace=True)) for i in range(n)]
-    lp, rp, m = (1-p)/2, 1-(1-p)/2, func(data)
-    return np.hstack([m, np.percentile(simulations, [lp*100, rp*100])])
-
-
-def CI_for_r(r, n, p=0.95):
-    '''
-    Bootstraping confidence intervals for the correlation coefficient
-    using Fisher transformation
-    
-    https://stats.stackexchange.com/questions/18887/how-to-calculate-a-confidence-interval-for-spearmans-rank-correlation
-    https://en.wikipedia.org/wiki/Fisher_transformation
-    '''
-
-    se = 1 / math.sqrt(n-3)
-    d = z_from_p(1-p) * se
-    return np.array([math.tanh(math.atanh(r) - d), math.tanh(math.atanh(r) + d)])
-
-
-def t_from_b(b, se): 
-    # t-statistic from beta-coefficient
-    return b/se
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def p_from_CI(estimate, CIL, CIR, alpha=0.95, method='two-tailed'):
+def p_from_ci(estimate, cil, cir, n=None, alpha=0.95, method="two-tailed"):
     # https://genometoolbox.blogspot.com/2013/11/how-to-estimate-p-value-from-confidence.html
     # z-critical = stats.norm.ppf(1 - alpha) (use alpha = alpha/2 for two-sided)
-    m = (CIR - CIL) / 2
-    se = m / z_from_p(1-alpha) # z-critical is used
-    z = estimate / se
-    p = p_from_z(z, method=method)
+
+    se = (cil - cir) / 2
+    if n is None:
+        se /= z_from_p(1 - alpha, method=method)  # z-critical is used
+        z = estimate / se
+        p = p_from_z(z, method=method)
+    else:
+        se /= t_from_p(1 - alpha, n, method=method)  # t-critical is used
+        t = estimate / se
+        p = p_from_t(t, n, method=method)
     return p
 
 
+def ci_from_r(r, n, alpha=0.95, method="two-tailed"):
+    """
+    Bootstraping confidence intervals for the correlation coefficient
+    using Fisher transformation
+    https://stats.stackexchange.com/questions/18887/how-to-calculate-a-confidence-interval-for-spearmans-rank-correlation
+    https://en.wikipedia.org/wiki/Fisher_transformation
+    """
 
-def cohen_d_from_f(f, df_num, df_denom): 
+    se = z_from_p(1 - alpha, method=method) / np.sqrt(n - 3)  # z-critical is used
+    return np.tanh(np.atanh(r) - se), np.tanh(np.atanh(r) + se)
+
+
+def ci_from_p(p, n, alpha=0.95, method="two-tailed"):
+    r = r_from_p(p, n, method=method)
+    return ci_from_r(r, n, alpha=alpha, method=method)
+
+
+def bootstrap(*args, func=np.mean, alpha=0.95, n_rep=1000, seed=13):
+    """
+    Bootstraping confidence intervals for the mean/median value
+    https://towardsdatascience.com/how-to-calculate-confidence-intervals-in-python-a8625a48e62b
+    """
+
+    np.random.seed(seed)
+    data = np.asanyarray(args)
+    idx = np.arange(0, data.shape[1], 1)
+    sample = [
+        func(*np.take(data, np.random.choice(idx, size=data.shape[1], replace=True), 1))
+        for _ in range(n_rep)
+    ]
+    lp, m, rp = (1 - alpha) / 2, 0.5, 1 - (1 - alpha) / 2
+    return np.percentile(sample, [lp * 100, m * 100, rp * 100])
+
+
+##############
+# Cohen r, d #
+##############
+
+
+def cohen_r_from_p(p, n, method="two-tailed"):
+    return r_from_p(p, n, method=method)
+
+
+def cohen_r_from_d(d):
+    return r_from_d(d)
+
+
+def cohen_r_from_z(z):
+    return r_from_z(z)
+
+
+def cohen_r_from_t(t, n):
+    return r_from_t(t, n)
+
+
+def cohen_d_from_p(p, n, method="two-tailed"):
+    return d_from_p(p, n, method=method)
+
+
+def cohen_d_from_r(r):
+    return d_from_r(r)
+
+
+def cohen_d_from_z(z):
+    return d_from_z(z)
+
+
+def cohen_d_from_t(t, n):
+    return d_from_t(t, n)
+
+
+def t_from_b(b, se):
+    # t-statistic from beta-coefficient
+    return b / se
+
+
+def cohen_d_from_f(f, df_num, df_denom):
     # Cohen’s d from F-statistic
     # Cohen’s d derived from F-statistic (Fritz et al., 2012)
     # Fritz, C. O., Morris, P. E., & Richler, J. J. (2012). Effect size estimates: Current use, calculations, and interpretation. Journal of Experimental Psychology: General, 141(1), 2–18. https://doi.org/10.1037/a0024338
     # https://www.researchgate.net/publication/51554230_Effect_Size_Estimates_Current_Use_Calculations_and_Interpretation
     # https://www.frontiersin.org/articles/10.3389/fpsyg.2013.00863/full
     eta_sq = (f * df_num) / (f * df_num + df_denom)
-    d = 2 * math.sqrt(eta_sq) / math.sqrt(1 - eta_sq)
+    d = 2 * np.sqrt(eta_sq) / np.sqrt(1 - eta_sq)
     return d
 
 
-def r_from_f(f, df): 
+def r_from_f(f, df):
     # Сorrelation r from F-statistic
     # https://juls-dotcom.github.io/meta_analysis.html
-    return f / math.sqrt(f + df)
+    return f / np.sqrt(f + df)
 
 
-def unbiased_z(z): 
+def unbiased_z(z):
     # unbiased Z
     # https://juls-dotcom.github.io/meta_analysis.html
     # sample sizes < 20 or 10 in each group, see Nakagawa & Cuthill, 2007
@@ -307,28 +317,28 @@ def unbiased_z(z):
 def logit_from_probit(x):
     # https://statmodeling.stat.columbia.edu/2006/06/06/take_logit_coef/
     # https://www.statalist.org/forums/forum/general-stata-discussion/general/1704819-calculating-odds-ration-in-probit-model
-    return -1.7*x
+    return -1.7 * x
 
 
-def OR_from_logit(x): 
+def OR_from_logit(x):
     # odds ratio from log odds ratio
     # odds = EXP(0.873) = 2.394;
     # https://stats.oarc.ucla.edu/other/mult-pkg/faq/general/faq-how-do-i-interpret-odds-ratios-in-logistic-regression/
     return np.exp(x)
 
 
-def logit_from_cohen_d(d): 
+def logit_from_cohen_d(d):
     # log odds ratio from Cohen's d
     # https://www.escal.site/
     # https://easystats.github.io/effectsize/reference/d_to_r.html
-    return np.pi*d / math.sqrt(3)
+    return np.pi * d / np.sqrt(3)
 
 
-def cohen_d_from_logit(x): 
+def cohen_d_from_logit(x):
     # Cohen's d from log odds ratio
     # https://stats.stackexchange.com/questions/68290/converting-odds-ratios-to-cohens-d-for-meta-analysis
     # https://www.um.es/metaanalysis/pdf/7078.pdf
-    return x * math.sqrt(3) / np.pi
+    return x * np.sqrt(3) / np.pi
 
 
 def logit_from_OR(x):
--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,255 +1,285 @@
-
+import numpy as np
+from scipy.stats import spearmanr
 import statsmodels.api as sm
 from metatools.calc import *
 
-def s(x):
-    return f'{x:.3f}'
 
-def test_calc():
+def s(x):
+    return f"{x:.3f}"
+
+
+def test_conversion():
     ##############
     # conversion #
     ##############
-    
+
     r = 0.75
     d = d_from_r(r)
-    assert s(d) == '2.268'
+    assert s(d) == "2.268"
     r = r_from_d(d)
-    assert s(r) == '0.750'
-    
+    assert s(r) == "0.750"
+
     r = 0.5
     z = z_from_r(r)
     r = r_from_z(z)
-    assert s(z) == '0.549'
-    assert s(r) == '0.500'
-    
+    assert s(z) == "0.549"
+    assert s(r) == "0.500"
+
     r = 0.9
     n = 100
     t = t_from_r(r, n)
     z = z_from_r(r)
-    assert s(t) == '20.440'
-    assert s(z) == '1.472'
+    assert s(t) == "20.440"
+    assert s(z) == "1.472"
     r = r_from_t(t, n)
-    assert s(r) == '0.900'
+    assert s(r) == "0.900"
     r = r_from_z(z)
-    assert s(r) == '0.900'   
+    assert s(r) == "0.900"
     d = d_from_t(t, n)
-    assert s(d) == '4.129'
+    assert s(d) == "4.129"
     r = r_from_d(d)
-    assert s(r) == '0.900'
+    assert s(r) == "0.900"
     d = d_from_z(z)
-    assert s(d) == '4.129'
+    assert s(d) == "4.129"
     r = r_from_d(d)
-    assert s(r) == '0.900'
+    assert s(r) == "0.900"
     z = z_from_d(d)
-    assert s(z) == '1.472'    
+    assert s(z) == "1.472"
     t = t_from_d(d, n)
-    assert s(t) == '20.440'
-    
-  
+    assert s(t) == "20.440"
+
     ###########
     # p-value #
-    ###########  
+    ###########
 
     r = -0.181
     n = 181
-    p = p_from_r(r, n, method='two-tailed')
-    assert s(p) == '0.015'
-    r = r_from_p(p, n, method='two-tailed')
-    assert s(r) == '0.181'  # does not keep sign 
-    
+    p = p_from_r(r, n, method="two-tailed")
+    assert s(p) == "0.015"
+    r = r_from_p(p, n, method="two-tailed")
+    assert s(r) == "0.181"  # does not keep sign
+
     d = -0.41
     n = 181
-    p = p_from_d(d, n, method='two-tailed') 
-    assert s(p) == '0.007'
-    d = d_from_p(p, n, method='two-tailed')
-    assert s(d) == '0.410' # does not keep sign   
-    
+    p = p_from_d(d, n, method="two-tailed")
+    assert s(p) == "0.007"
+    d = d_from_p(p, n, method="two-tailed")
+    assert s(d) == "0.410"  # does not keep sign
+
     t = -2.16
     n = 163
-    p = p_from_t(t, n, method='two-tailed')
-    assert s(p) == '0.032'
-    t = t_from_p(p, n, method='two-tailed')
-    assert s(t) == '2.160'
+    p = p_from_t(t, n, method="two-tailed")
+    assert s(p) == "0.032"
+    t = t_from_p(p, n, method="two-tailed")
+    assert s(t) == "2.160"
     t = 2.16
-    p = p_from_t(t, n, method='two-tailed')
-    assert s(p) == '0.032'
-    t = t_from_p(p, n, method='two-tailed')
-    assert s(t) == '2.160'
-    
+    p = p_from_t(t, n, method="two-tailed")
+    assert s(p) == "0.032"
+    t = t_from_p(p, n, method="two-tailed")
+    assert s(t) == "2.160"
+
     p = 0.5
-    z = z_from_p(p, method='two-tailed')
-    p = p_from_z(z, method='two-tailed')
-    assert s(z) == '0.674'
-    assert s(p) == '0.500'    
+    z = z_from_p(p, method="two-tailed")
+    p = p_from_z(z, method="two-tailed")
+    assert s(z) == "0.674"
+    assert s(p) == "0.500"
     z = -2.335
-    p = p_from_z(z, method='two-tailed')
-    assert s(p) == '0.020'
+    p = p_from_z(z, method="two-tailed")
+    assert s(p) == "0.020"
     z = 2.052
-    p = p_from_z(z, method='two-tailed')
-    assert s(p) == '0.040'
-    z = z_from_p(p, method='two-tailed')
-    assert s(z) == '2.052'
-    
+    p = p_from_z(z, method="two-tailed")
+    assert s(p) == "0.040"
+    z = z_from_p(p, method="two-tailed")
+    assert s(z) == "2.052"
+
     n = 163
     z = -2.335
-    p = p_from_z(z, method='two-tailed')
-    t = t_from_p(p, n, method='two-tailed')
-    p = p_from_t(t, n, method='two-tailed')
-    assert s(p) == '0.020'
+    p = p_from_z(z, method="two-tailed")
+    t = t_from_p(p, n, method="two-tailed")
+    p = p_from_t(t, n, method="two-tailed")
+    assert s(p) == "0.020"
     z = z_from_p(p)
-    assert s(z) == '2.335'
-    
+    assert s(z) == "2.335"
+
     t = -2.16
-    p = p_from_t(t, n, method='two-tailed')
-    z = z_from_p(p, method='two-tailed')
-    p = p_from_z(z, method='two-tailed')
-    assert s(p) == '0.032'
-    t = t_from_p(p, n, method='two-tailed')
-    assert s(t) == '2.160'   
-    
+    p = p_from_t(t, n, method="two-tailed")
+    z = z_from_p(p, method="two-tailed")
+    p = p_from_z(z, method="two-tailed")
+    assert s(p) == "0.032"
+    t = t_from_p(p, n, method="two-tailed")
+    assert s(t) == "2.160"
+
     d = 2.5
     n = 10
     t = t_from_d(d, n)
-    assert s(t) == '3.536'
+    assert s(t) == "3.536"
     d = d_from_t(t, n)
-    assert s(d) == '2.500'  
+    assert s(d) == "2.500"
     z = z_from_t(t, n)
     d = d_from_z(z)
-    assert s(d) == '2.500' 
-    
+    assert s(d) == "2.500"
+
     d = 2.5
     n = 10
     z = z_from_d(d)
-    assert s(z) == '1.048'
+    assert s(z) == "1.048"
     d = d_from_z(z)
-    assert s(d) == '2.500'  
+    assert s(d) == "2.500"
     t = t_from_z(z, n)
     d = d_from_t(t, n)
-    assert s(d) == '2.500'   
-    
+    assert s(d) == "2.500"
+
     r = 0.9
     n = 10
     t = t_from_r(r, n)
-    assert s(t) == '5.840'
+    assert s(t) == "5.840"
     r = r_from_t(t, n)
-    assert s(r) == '0.900'  
+    assert s(r) == "0.900"
     z = z_from_t(t, n)
     r = r_from_z(z)
-    assert s(r) == '0.900'   
-    
+    assert s(r) == "0.900"
+
     r = 0.90
     n = 10
     z = z_from_r(r)
-    assert s(z) == '1.472'
+    assert s(z) == "1.472"
     r = r_from_z(z)
-    assert s(r) == '0.900'  
+    assert s(r) == "0.900"
     t = t_from_z(z, n)
     r = r_from_t(t, n)
-    assert s(r) == '0.900'  
- 
+    assert s(r) == "0.900"
+
     p = 0.010
     n = 10
-    t = t_from_p(p, n, method='two-tailed')
-    assert s(t) == '3.355'
-    p = p_from_t(t, n, method='two-tailed')
-    assert s(p) == '0.010'   
-    z = z_from_t(t, n, method='two-tailed')
-    p = p_from_z(z, method='two-tailed')
-    assert s(p) == '0.010' 
- 
+    t = t_from_p(p, n, method="two-tailed")
+    assert s(t) == "3.355"
+    p = p_from_t(t, n, method="two-tailed")
+    assert s(p) == "0.010"
+    z = z_from_t(t, n, method="two-tailed")
+    p = p_from_z(z, method="two-tailed")
+    assert s(p) == "0.010"
+
     p = 0.010
     n = 10
-    z = z_from_p(p, method='two-tailed')
-    assert s(z) == '2.576'
-    p = p_from_z(z, method='two-tailed')
-    assert s(p) == '0.010'  
-    t = t_from_z(z, n, method='two-tailed')
+    z = z_from_p(p, method="two-tailed")
+    assert s(z) == "2.576"
+    p = p_from_z(z, method="two-tailed")
+    assert s(p) == "0.010"
+    t = t_from_z(z, n, method="two-tailed")
     p = p_from_t(t, n)
-    assert s(p) == '0.010' 
- 
+    assert s(p) == "0.010"
+
     # sign check
-    
+
     r = -0.9
     n = 10
     t = t_from_r(r, n)
-    assert s(t) == '-5.840'
+    assert s(t) == "-5.840"
     r = r_from_t(t, n)
-    assert s(r) == '-0.900'  
+    assert s(r) == "-0.900"
     z = z_from_t(t, n)
-    assert s(z) == '-1.472'
+    assert s(z) == "-1.472"
     r = r_from_z(z)
-    assert s(r) == '-0.900' 
-    
+    assert s(r) == "-0.900"
+
     r = -0.90
     n = 10
     z = z_from_r(r)
-    assert s(z) == '-1.472'
+    assert s(z) == "-1.472"
     r = r_from_z(z)
-    assert s(r) == '-0.900'  
+    assert s(r) == "-0.900"
     t = t_from_z(z, n)
-    assert s(t) == '-5.840'
+    assert s(t) == "-5.840"
     r = r_from_t(t, n)
-    assert s(r) == '-0.900'
-    
+    assert s(r) == "-0.900"
+
     d = -2.5
     n = 10
     t = t_from_d(d, n)
-    assert s(t) == '-3.536'
+    assert s(t) == "-3.536"
     d = d_from_t(t, n)
-    assert s(d) == '-2.500'  
+    assert s(d) == "-2.500"
     z = z_from_t(t, n)
-    assert s(z) == '-1.048'
+    assert s(z) == "-1.048"
     d = d_from_z(z)
-    assert s(d) == '-2.500' 
-    
+    assert s(d) == "-2.500"
+
     d = -2.5
     n = 10
     z = z_from_d(d)
-    assert s(z) == '-1.048'
+    assert s(z) == "-1.048"
     d = d_from_z(z)
-    assert s(d) == '-2.500'  
+    assert s(d) == "-2.500"
     t = t_from_z(z, n)
-    assert s(t) == '-3.536'
+    assert s(t) == "-3.536"
     d = d_from_t(t, n)
-    assert s(d) == '-2.500' 
+    assert s(d) == "-2.500"
 
-    
-    
-    #print(r)
-     
-    '''
-    
-    
-    r = 0.5
-    n = 20
-    z = z_from_r(r)
-    assert s(z) == '0.549'
-    t = t_from_z(z, n)
-    print('t_from_z', t)
-    d = d_from_t(t, n)
-    print('d_from_t', d)
-    r = r_from_d(d)
-    print(r)
-    assert s(r) == '0.500'    
-    
-    
-    
-    print('\n')
+    p = p_from_r(-0.79056, 5)
+    assert (
+        s(p) == "0.111"
+    )  # spearman(statistic=-0.7905694150420948, pvalue=0.11136715471408384)
 
-    t = t_from_z(z, n)
-    p = p_from_t(t, n, method='two-tailed')
-    print('p_from_t', p)
-    print('t_from_z', t)
-    
-    t = -2.16
-      
-    z = z_from_t(t, n)
-    p = p_from_z(z, method='two-tailed')
-    print('z_from_t', z)
-    print('p_from_z', p)
-    '''
+    p = p_from_ci(
+        6.37,
+        0.773,
+        11.959,
+    )
+    assert s(p) == "0.026"  # 0.028
+    p = p_from_ci(6.37, 0.773, 11.959, 88)
+    assert s(p) == "0.026"  # 0.028
 
-    '''    
+    cil, cir = ci_from_r(0.95, 100)
+    assert s(cil) == "0.926"
+    assert s(cir) == "0.966"
+
+    cil, cir = ci_from_r(0.053, 198)
+    p = p_from_ci(0.053, -0.087, 0.191, 198)
+    assert s(cil) == "-0.087"
+    assert s(cir) == "0.191"  # 0.206
+    assert s(p) == "0.453"  # 0.553
+
+    cil, cir = ci_from_p(p, 198)
+    assert s(cil) == "-0.086"
+    assert s(cir) == "0.192"
+
+
+def test_bootstrap():
+    cil, m, cir = bootstrap(
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        func=np.mean,
+        n_rep=1000,
+        seed=13,
+    )
+    assert s(cil) == "3.333"
+    assert s(m) == "5.000"
+    assert s(cir) == "6.667"
+
+    def f(*args):
+        return spearmanr(*args).statistic
+
+    cil, m, cir = bootstrap(
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [4, 5, 8, 4, 5, 6, 7, 8, 9],
+        func=f,
+        n_rep=1000,
+        seed=13,
+    )
+    assert s(cil) == "0.029"
+    assert s(m) == "0.745"
+    assert s(cir) == "1.000"
+
+    p = p_from_r(m, 9)
+    cil, cir = ci_from_r(m, 9)
+    assert s(cil) == "0.159"
+    assert s(p) == "0.021"
+    assert s(cir) == "0.943"
+
+    # spearmanr
+    # SignificanceResult(statistic=np.float64(0.7173570516842638),
+    # pvalue=np.float64(0.02958657594620456))
+
+    """    
     test_data = sm.datasets.get_rdataset("mtcars", "datasets", cache=True).data
     assert len(test_data) == 32
 
@@ -290,7 +320,9 @@ def test_calc():
     assert results_APA[2].iloc[0]['model'] == 'R² = .99, R²adj = .99, R²pred = .80, F(2, 30) = 1520.63, p < .001'
 
     print('Tests for metatools.lm are PASSED!')
-    '''
-    
-if __name__ == '__main__':
-    test_calc()
+    """
+
+
+if __name__ == "__main__":
+    test_conversion()
+    test_bootstrap()
