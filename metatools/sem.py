d4b4921d05ca393c58e61eea5264eef1bcbd917c
Danila <d.v.valko@gmail.com>
2024-12-24 02:54:07 +0100

    modified:   metatools/sem.py

--- metatools/sem.py
+++ metatools/sem.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import semopy
+import os
 
 
 from .format import format_p
@@ -8,10 +9,15 @@ from .format import format_p
 # it uses graphviz. for windows, use https://graphviz.gitlab.io/download/
 # https://arxiv.org/pdf/2106.01140.pdf
 # https://semopy.com/cite.html
+# https://gitlab.com/georgy.m/semopy
+# ~ to specify structural part,
+# =~ to specify measurement part,
+# ~~ to specify common variance between variables.
 # formula = f"""
-# ПП =~ {' + '.join(p)}
-# НЭ =~ {' + '.join(e)}
-# ПП ~ НЭ + {' + '.join(x)} + Группа
+# x1 ~ x2 + x3
+# x3 ~ x2 + eta1
+# eta1 =~ y1 + y2 + y3
+# eta1 ~ x1
 # """
 
 
@@ -21,8 +27,8 @@ def sem(
     method="MLW",  # MLW ULS GLS FIML DWLS WLS
     solver="SLSQP",
     bootstrap=100,
-    se_robust=True,
-    standardized=True,
+    se_robust=True,  # Huber-White correction
+    return_model=False,
     seed=13,
 ):
     np.random.seed(seed)
@@ -32,9 +38,11 @@ def sem(
         semopy.bias_correction(model, n=bootstrap, resample_mean=True)
     metrics = semopy.calc_stats(model).T.rename(columns={"Value": "value"})
     metrics = pd.concat([pd.Series({"N": data.shape[0]}, name="value"), metrics])
-    stats = model.inspect(std_est=standardized, se_robust=se_robust)
+    stats = model.inspect(std_est=True, se_robust=se_robust)
     stats.columns = [i.lower().replace(". ", "_") for i in stats.columns]
     stats = stats.replace("-", np.nan).infer_objects(copy=False)
+    if return_model:
+        return stats, metrics, model
     return stats, metrics
 
 
@@ -42,6 +50,7 @@ def sem_report(stats, metrics, decimal=3, format_pval=True):
     # https://people.ucsc.edu/~zurbrigg/psy214b/09SEM8a.pdf
     # χ2(48, N = 500) = 303.80, p < .001, TLI = .86, CFI = .90
 
+    stats = stats.copy()
     if format_pval:
         stats["p-value"] = (
             stats["p-value"]
@@ -55,13 +64,57 @@ def sem_report(stats, metrics, decimal=3, format_pval=True):
     r = metrics.iloc[:6].T.to_dict()
     m = metrics.iloc[6:11].T.to_dict()
     r = f"χ2({int(r['DoF']['value'])}, N = {int(r['N']['value'])}) = {round(r['chi2']['value'], decimal)}, {format_p(r['chi2 p-value']['value'])}"
-    m = ", ".join([f"{i} = " + f"{round(v['value'], decimal)}"[1:] for i, v in m.items()])
+    m = ", ".join(
+        [f"{i} = " + f"{round(v['value'], decimal)}"[1:] for i, v in m.items()]
+    )
     r = r + ", " + m
     m = metrics.iloc[11:].T.to_dict()
     m = ", ".join([f"{i} = " + f"{round(v['value'], decimal)}" for i, v in m.items()])
     stats = stats.round(decimal)
     stats.loc[stats.index[0], "model"] = r + ", " + m
     return stats
+
+
+def sem_plot(
+    stats,
+    save_to_file,
+    plot_covs=True,
+    std_ests=True,
+    format_pval=True,
+    format_fig="pdf",
+    method="semopy",
+    return_fig=True,
+    show_fig=False,
+    decimal=3,
+    dpi=600,
+    model=None,
+):
+    #
+    if method == "semopy":
+        if model is None:
+            raise AssertionError(
+                "For the semopy method, the model parameter is necessary!"
+            )
+        import tempfile
+        import shutil
+
+        tfile = os.path.join(tempfile.gettempdir(), f"sem_tmp_fig.{format_fig}")
+        inspection = stats.copy()
+        inspection["Estimate"] = inspection["estimate"]
+        inspection["Est. Std"] = inspection["est_std"]
+        inspection["p-value"] = inspection["p-value"].fillna("-")
+        fig = semopy.semplot(
+            model,
+            tfile,
+            plot_covs=plot_covs,
+            std_ests=std_ests,
+            inspection=inspection,
+            show=False,
+        )
+        shutil.copy(tfile, save_to_file)
+        if return_fig:
+            return fig
+
 
 '''
 def _semplot(model, save_to_file, plot_covs, std_ests):
--- tests/test_sem.py
+++ tests/test_sem.py
@@ -1,6 +1,7 @@
 import semopy
+import os
 
-from metatools.sem import sem, sem_report
+from metatools.sem import sem, sem_report, sem_plot
 
 
 def test_sem():
@@ -9,25 +10,40 @@ def test_sem():
     formula = semopy.examples.political_democracy.get_model()
     assert len(data) == 75
 
-    stats, metrics = sem(
+    stats, metrics, model = sem(
         data,
         formula,
         method="MLW",  # MLW ULS GLS FIML DWLS WLS
         solver="SLSQP",
         bootstrap=10,
         se_robust=True,
-        standardized=True,
+        return_model=True,
         seed=13,
     )
 
     assert f"{stats['estimate'].iloc[0]:.3f}" == "1.252"
 
-    stats = sem_report(stats, metrics)
+    results = sem_report(stats, metrics)
     assert (
-        stats.iloc[0]["model"]
+        results.iloc[0]["model"]
         == "χ2(35, N = 75) = 38.125, p = .329, CFI = .995, GFI = .948, AGFI = .918, NFI = .948, TLI = .993, RMSEA = 0.035, AIC = 60.872, BIC = 132.714, LogLik = 0.564"
     )
-    print(stats)
+    print(results)
+
+    fig = sem_plot(
+        stats,
+        save_to_file="fig.pdf",
+        plot_covs=True,
+        std_ests=True,
+        format_fig="pdf",
+        method="semopy",
+        return_fig=True,
+        show_fig=False,
+        dpi=600,
+        model=model,
+    )
+
+    assert fig is not None
 
 
 if __name__ == "__main__":
